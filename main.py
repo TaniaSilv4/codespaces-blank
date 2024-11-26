6f3f64a235694058c2ec455ecc92de0cf0351b02
TaniaSilv4 <tania.silva@genstudents.org>
2024-11-26 15:35:20 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+ #lee la variable, le asigna un valor y la guardia en memoria
+
+nombre = input("Ingresa tu nombre: ") #imprime un mensaje al usuario y espera a que ingrese un dato
+apellido = input("Ingresa tu apellido : ") 
+ciudad = input("¿En que ciudad vives?: ") 
+
+
+print(f"Hola, {nombre} {apellido}. seleccionaste la cuidad: {ciudad}.")
+
+
+nombre_completo = f"{nombre} {apellido}"
+print(f"Nombre completo: {nombre_completo}")
