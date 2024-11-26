cae6ff2d274b3eead833b7916599cafba31130fc
TaniaSilv4 <tania.silva@genstudents.org>
2024-11-26 16:10:09 +0000

    nuevas lineas

--- main.py
+++ main.py
@@ -4,12 +4,16 @@
 nombre = input("Ingresa tu nombre: ") #imprime un mensaje al usuario y espera a que ingrese un dato
 apellido = input("Ingresa tu apellido : ") 
 ciudad = input("¿En que ciudad vives?: ") 
-
-
-print(f"Hola, {nombre} {apellido}. seleccionaste la cuidad: {ciudad}.")
-
-
 nombre_completo = f"{nombre} {apellido}"
 print(f"Nombre completo: {nombre_completo}")
-#nuevo 
-print
+print(f"Hola, {nombre} {apellido}. quien vive en: {ciudad}. Puedes consultar")
+
+def sumar(x, y):
+    return x + y
+#solicitar que el usuario que ingrese dos numeros
+numero1 = float(input("introduce el primer numero: "))
+numero2 = float(input("introduce el segundo numero:"))
+#calcular la suma
+resultado = sumar(numero1, numero2)
+#mostrar resultado
+print(f"tu resultado: {resultado}" )
